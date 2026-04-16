33ae94a2f846827e3403b0bfdc8b1700f0ac62a6
Schulien-J <jj.konrad.de@gmail.com>
2026-04-16 15:55:35 +0200

    Tests are now found in the monitored repo

--- tests/test_main.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from main import dummy
-
-
-@pytest.mark.timeout(5)
-def test_dummy():
-    assert dummy() == 8
--- tests/testing_libs.py
+++
@@ -1,7 +0,0 @@
-import json 
-
-cache = {}
-with open("branch_cache.json", "r") as f:
-    cache = json.load(f)
-
-print(cache)
